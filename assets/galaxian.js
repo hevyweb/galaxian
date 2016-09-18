86d794caf37133b26692653e1c06701e34cc89ef
Dmytro_Dzyuba <1932@bk.ru>
2016-09-19 00:03:46 +0300

    some mess

--- assets/galaxian.js
+++ assets/galaxian.js
@@ -1,17 +1,17 @@
 $(function() {
     var enemyInterface = {
         fire: function(){},
-        fly: function(){},
+        fly: function(){}
     };
     
     var enemyBuilder = {
         types: {},
         
-        build: function(type){console.log(type);
+        build: function(type){
             var place = $('<div />').addClass('gx_enemy_place');
             if (type != null){
                 if (this.types[type] === undefined){
-                    this.types[type] = $('<div />').addClass('gx_enemy_' + type);
+                    this.types[type] = $('<div />').addClass('gx_enemy gx_enemy_' + type);
                 }
                 this.types[type].clone().appendTo(place);
             }
@@ -22,7 +22,7 @@ $(function() {
     var keys = {
         left: 37,
         //top: 38,
-        right: 39,
+        right: 39
         //bottom: 40
     };
     
@@ -33,11 +33,13 @@ $(function() {
         ['soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier'],
         ['soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier'],
         ['soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier','soldier']
-    ]
+    ];
     
     var GalaxianGame = {
         
         missile: null,
+        
+        map: [],
         
         init: function(container){
             this.container = $(container);
@@ -84,10 +86,29 @@ $(function() {
             this.missile.css({
                 'top': position.top,
                 'left': position.left
-            }).animate({'top': 0}, 2000, 'linear', function(){
-                $(this).remove();
-                self.missile = null;
-                $('.gx_missile').show();
+            }).animate({'top': 0},{
+                duration: 2000,
+                easing: 'linear',
+                complete: function(){
+                    $(this).remove();
+                    self.missile = null;
+                    $('.gx_missile').show();
+                },
+                step: function(Y){
+                    var X1 = parseInt($(this).offset().left);
+                    var X2 = X1 + $(this).width();
+                    for(;X1<=X2; X1++ ){
+                        if (self.map[X1] != undefined || self.map[X1] != null){
+                            var enemy = self.map[X1];
+                            if ((enemy.offset().top + enemy.height())>=Y){
+                                self.map[X1] = null;
+                                self.rebuildMapFragment(self.map[X1]);
+                                enemy.remove();
+                                $(this).finish();
+                            }
+                        }
+                    }
+                }
             });
         },
         
@@ -100,6 +121,45 @@ $(function() {
             this.buildStage();
             this.buildHero();
             this.buildEnemy();
+            var self = this;
+            $('.gx_enemy').each(function(key, enemy){
+                self.rebuildPosMap($(enemy));
+            });                                             
+        },
+        
+        rebuildPosMap: function(o)
+        {
+            var coordinates = o.offset();
+            var x1 = coordinates.left;
+            var x2 = x1 + o.width();
+            var y = coordinates.top + o.height();
+            var cache;
+            var cacheY;
+            for(;x1<=x2;x1++){
+                if (this.map[x1] !== undefined && this.map[x1] !== null){
+                    if (!this.map[x1][0] !== o[0]){
+                        if (cache == null || !this.map[x1][0] !== cache[0]){
+                            cache = this.map[x1];
+                            cacheY = cache.offset().top + cache.height();
+                        }
+                        if  (cacheY < y){
+                            this.map[x1] = o;
+                        }
+                    }
+                } else {
+                    this.map[x1] = o;
+                }
+            }
+        },
+        
+        rebuildMapFragment: function(fragment)
+        {
+            
+            var X1 = fragment.offset().left;
+            var X2 = X1 + fragment.width();
+            for (;X1<=X2;X1++){
+                this.map[X1] = null;
+            }
         }
     };
     
@@ -107,3 +167,9 @@ $(function() {
     GalaxianGame.start();
 });
 
+$.fn.rect = function(){
+    var data = this.offset();
+    data.w = this.width();
+    data.h = this.height();
+    return data;
+};
